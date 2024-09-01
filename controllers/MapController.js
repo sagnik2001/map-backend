39989a77f606a230b4f145275b9429a1e9f337e8
Sagnik Pal <palsagnik2001@gmail.com>
2024-09-01 14:42:57 +0530

    Added Caching Mechanism

--- controllers/MapController.js
+++ controllers/MapController.js
@@ -1,4 +1,17 @@
-const Map = require("../schemas/mapModel")
+const redis = require('redis');
+require('dotenv').config()
+const client = redis.createClient({
+    url: process.env.REDIS_URL
+});
+
+
+client.on('error', (err) => {
+    console.error('Redis error:', err);
+});
+
+client.connect();  // Use connect() if using Redis v4.x or higher
+
+const Map = require("../schemas/mapModel");
 
 class MapController {
     async saveMapData(req, res) {
@@ -11,9 +24,13 @@ class MapController {
                 mapData: { latitude, longitude }
             });
             await mapData.save();
+
+            const cacheKey = `topRegions:${userId}`;
+            await client.del(cacheKey);
+
             return res.status(201).json({ status: 201, data: { mapData: mapData }, message: 'Map Saved Successfully' });
         } catch (error) {
-            res.status(400).json({ message: error.message });
+            return res.status(400).json({ message: error.message });
         }
     }
 
@@ -29,7 +46,14 @@ class MapController {
 
     async getTopFrequentReg(req, res) {
         const userId = req.userId;
+        const cacheKey = `topRegions:${userId}`;
+
         try {
+            const cachedData = await client.get(cacheKey);
+            if (cachedData) {
+                return res.status(200).json({ status: 200, data: JSON.parse(cachedData), message: 'Top Frequent Regions (from cache)' });
+            }
+
             const topRegions = await Map.aggregate([
                 { $match: { userId } },
                 {
@@ -45,7 +69,7 @@ class MapController {
                 { $sort: { count: -1 } },
                 { $limit: 3 }
             ]);
-    
+
             const result = topRegions.map(region => ({
                 mapData: {
                     latitude: region._id.latitude,
@@ -54,13 +78,16 @@ class MapController {
                 count: region.count,
                 imageUrl: region.imageUrl
             }));
-    
+
+            await client.set(cacheKey, JSON.stringify(result), {
+                EX: 3600,
+            });
+
             return res.status(200).json({ status: 200, data: result, message: 'Top Frequent Regions' });
         } catch (error) {
-            res.status(500).json({ message: error.message });
+            return res.status(500).json({ message: error.message });
         }
     }
-    
 }
 
 module.exports = new MapController();
